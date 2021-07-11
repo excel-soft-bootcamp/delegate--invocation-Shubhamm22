3c1cee61540727729251af94b3aed2b05272e5f3
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-11 09:36:35 +0530

    Add files via upload

---
+++ DelegateInvocation/Operations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateInvocation
+{
+
+
+    public class Operations
+    {
+        public static double Calculate(int x, double y) { return x + y; }
+
+        public void Update(int newNumber) { System.Console.WriteLine(newNumber); }
+
+        public void Delete(string key) { }
+        public void ZeroArguments() { }
+        public void String(string data) { }
+        public int Integer(int x)
+        {
+            return x;
+        }
+        public bool StringBoolean(string data, string newData)
+        {
+            return true;
+        }
+
+        public static void FunctionsAsAnArguments(Action obj, Action<string> obj2, Func<int, int> obj3, Func<string, string, bool> obj4, Func<int, double, double> obj7, Action<int> obj8)
+        {
+            obj.Invoke();
+            obj2.Invoke("hello");
+            int intValue = obj3.Invoke(111);
+            bool boolValue = obj4.Invoke("Good Morning", "Good Evening");
+            double doubleValue = obj7.Invoke(55, 7.5);
+            obj8.Invoke(3);
+
+            Console.WriteLine(intValue);
+            Console.WriteLine(boolValue);
+            Console.WriteLine(doubleValue);
+        }
+       
+    }
+
+        
+         
+
+}
---
+++ DelegateInvocation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateInvocation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations _operations = new Operations();
+            
+            Func<int, double, double> calculatePtr = new Func<int, double, double>(Operations.Calculate);
+            Action<int> updatePtr = new Action<int>(_operations.Update);
+            Action<string> deletePtr = new Action<string>(_operations.Delete);
+            Action action = new Action(_operations.ZeroArguments);
+            Action<string> stringPtr = new Action<string>(_operations.String);
+            Func<int, int> integerPtr = new Func<int, int>(_operations.Integer);
+            Func<string, string, bool> boolPtr = new Func<string, string, bool>(_operations.StringBoolean);
+
+            Operations.FunctionsAsAnArguments(action, stringPtr, integerPtr, boolPtr, calculatePtr, updatePtr);
+
+        }
+    }
+}
